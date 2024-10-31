63e98d3b26c57d0ab289c31e349d2f39ce9156b5
javalce <javalce29@gmail.com>
2024-10-31 14:58:53 +0100

    Configure ESLint overrides for Vitest type checking and adjust specific rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,17 @@ import { defineConfig } from '@javalce/eslint-config';
 export default defineConfig({
   testing: 'vitest',
   type: 'lib',
+  overrides: [
+    {
+      settings: {
+        vitest: {
+          typecheck: true,
+        },
+      },
+      rules: {
+        'no-nested-ternary': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+      },
+    },
+  ],
 });
